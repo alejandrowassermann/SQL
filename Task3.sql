f78f15a6abc9ed6f209d77284830445e738d650a
Alejandro Wassermann <alewacapo@gmail.com>
2021-01-27 23:45:10 -0300

    adding task3 solution

--- Task3.sql
+++ Task3.sql
@@ -5,3 +5,12 @@
 --2019-01     California      50
 
 --2019-02     California      45
+
+SELECT 
+FORMAT(US.created_at_timestamp, 'yyyy-MM') as Month,
+LOC.State as State,
+COUNT(US.user_id) as [# of New Users]
+FROM [dbo].[User] US
+INNER JOIN dbo.Location LOC ON US.user_id = LOC.user_id
+GROUP BY FORMAT(US.created_at_timestamp, 'yyyy-MM'), LOC.State
+ORDER BY LOC.State,FORMAT(US.created_at_timestamp, 'yyyy-MM')  ASC

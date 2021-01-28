1eaef71c236eef02c1ab57268169c3a6dcfb5fdf
Alejandro Wassermann <alewacapo@gmail.com>
2021-01-27 23:38:03 -0300

    decided to use format instead of convert for the date manipulation

--- Task2.sql
+++ Task2.sql
@@ -1,7 +1,4 @@
 
-
-
-
 --2) By day, show the total number of registrations, comments, and posts created. The output should look like the following:
 --
 --Date            # of Registrations      # of Comments       # of Posts Created
@@ -9,3 +6,13 @@
 --2019-08-01      50                      10                  20
 
 --2019-08-02      43                      13                  13
+
+Select 
+FORMAT(event_time, 'yyyy-MM-dd') as [Date], --convert the datetime to the requested format
+SUM(CASE WHEN event = 'register'  THEN 1 END) as [# of Registrations],
+SUM(CASE WHEN event = 'comment'  THEN 1 END) as [# of Comments],
+SUM(CASE WHEN event = 'createPost'  THEN 1 END) as [# of Posts Created]
+
+ from [dbo].[Actions]
+ GROUP BY FORMAT(event_time, 'yyyy-MM-dd') 
+ ORDER BY FORMAT(event_time, 'yyyy-MM-dd') asc
---
+++ Task3.sql
@@ -0,0 +1,7 @@
+--3) Show the number of new users by month and state. Here's a sample output:
+
+--Month       State           # of New Users
+
+--2019-01     California      50
+
+--2019-02     California      45

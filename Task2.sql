b91b7b3a2696155ab585173122e49b469bb35edc
Alejandro Wassermann <alewacapo@gmail.com>
2021-01-27 23:26:50 -0300

    adding proposed solution to task2

---
+++ Task2.sql
@@ -0,0 +1,11 @@
+
+
+
+
+--2) By day, show the total number of registrations, comments, and posts created. The output should look like the following:
+--
+--Date            # of Registrations      # of Comments       # of Posts Created
+
+--2019-08-01      50                      10                  20
+
+--2019-08-02      43                      13                  13
